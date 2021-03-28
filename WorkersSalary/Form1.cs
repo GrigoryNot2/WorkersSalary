d846b4e53060518c53e39067663b3acdce1086e4
GrigoryNot2 <65012726+GrigoryNot2@users.noreply.github.com>
2021-03-28 23:59:41 +0300

    2

--- WorkersSalary/Form1.cs
+++ WorkersSalary/Form1.cs
@@ -21,17 +21,18 @@ namespace WorkersSalary
             using (var connection = new SqliteConnection(connectionString))
             {
                 connection.Open();
-
                 SqliteCommand command = new SqliteCommand();
                 command.CommandText = "CREATE TABLE IF NOT EXISTS workers(_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, Tn INTEGER NOT NULL UNIQUE, Name TEXT NOT NULL)";
-                
                 command.Connection = connection;
-
-                int number = command.ExecuteNonQuery();
-
+                Int64 number = command.ExecuteNonQuery();
+                command.CommandText = "SELECT COUNT(*) FROM workers";
+                number = (Int64)command.ExecuteScalar();
                 richTextBox1.Text = $"{number}";
+
+                command.CommandText = "INSERT INTO workers(Tn, Name) VALUES (111112, 'Josh'), (111113, 'Nikc')";
+                number = command.ExecuteNonQuery();
+                richTextBox1.Text = $"\n{number}";
             }
         }
-
     }
 }
