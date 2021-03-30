505456ccd751448318fec76b1c8a74e295cdebc7
Admini <Admini@DESKTOP-O1TC02K>
2021-03-30 10:00:53 +0300

    1

--- WorkersSalary/Form1.cs
+++ WorkersSalary/Form1.cs
@@ -22,18 +22,38 @@ namespace WorkersSalary
             {
                 connection.Open();
                 SqliteCommand command = new SqliteCommand();
-                command.CommandText = "CREATE TABLE IF NOT EXISTS workers(_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, Tn INTEGER NOT NULL UNIQUE, Name TEXT NOT NULL)";
+                command.CommandText = "CREATE TABLE IF NOT EXISTS Workers(" +
+                                      "_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE," +
+                                      "Tn INTEGER NOT NULL UNIQUE," +
+                                     //"Tn INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,"+
+                                      "Name TEXT NOT NULL" +
+                                      ")";
                 command.Connection = connection;
                 Int64 number = command.ExecuteNonQuery();
                 command.CommandText = "SELECT COUNT(*) FROM workers";
                 number = (Int64)command.ExecuteScalar();
-                richTextBox1.Text = $"{number}";
+                richTextBox1.Text = $"Количество строк в Workers: {number}";
                 if (number == 0)
                 {
-                    command.CommandText = "INSERT INTO workers(Tn, Name) VALUES (111112, 'Josh'), (111113, 'Nikc')";
+                    command.CommandText = "INSERT INTO workers(Tn, Name) " +
+                                          "VALUES (111112, 'Josh'), (111113, 'Nikc')";
                     number = command.ExecuteNonQuery();
-                    richTextBox1.Text = $"\n{number}";
+                    richTextBox1.Text = $"\nДобавлено строк в Workers: {number}";
                 }
+                command.CommandText = "CREATE TABLE IF NOT EXISTS Salary(" +
+                                      "_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE," +
+                                      "Tn INTEGER NOT NULL," +
+                                      "Salary REAL NOT NULL," +
+                                      //"Data" +
+                                      "FOREIGN KEY (Tn)" +
+                                      "   REFERENCES Workers (Tn)" +
+                                      "       ON UPDATE CASCADE" +
+                                      "       ON DELETE CASCADE" +
+                                      ")";
+                command.ExecuteNonQuery();
+                command.CommandText = "SELECT COUNT(*) FROM Salary";
+                number = (Int64)command.ExecuteScalar();
+                richTextBox1.Text = $"Количество строк в Salary: {number}";
             }
 
         }
