7c1f1d48627808bc48085bd5ec4156ebf06b1ba4
Admini <Admini@DESKTOP-O1TC02K>
2021-04-02 00:32:09 +0300

    1

--- WorkersSalary/Form1.Designer.cs
+++ WorkersSalary/Form1.Designer.cs
@@ -38,44 +38,52 @@ namespace WorkersSalary
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(2, 363);
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 638);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(700, 87);
+            this.richTextBox1.Size = new System.Drawing.Size(582, 115);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             // 
             // dataGridWorkers
             // 
+            this.dataGridWorkers.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridWorkers.BackgroundColor = System.Drawing.SystemColors.Window;
             this.dataGridWorkers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridWorkers.Location = new System.Drawing.Point(2, 1);
-            this.dataGridWorkers.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridWorkers.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridWorkers.Location = new System.Drawing.Point(0, 0);
             this.dataGridWorkers.Name = "dataGridWorkers";
             this.dataGridWorkers.RowHeadersWidth = 51;
             this.dataGridWorkers.RowTemplate.Height = 29;
-            this.dataGridWorkers.Size = new System.Drawing.Size(699, 182);
+            this.dataGridWorkers.Size = new System.Drawing.Size(582, 243);
             this.dataGridWorkers.TabIndex = 1;
             this.dataGridWorkers.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridWorkers_CellClick);
             this.dataGridWorkers.DataBindingComplete += new System.Windows.Forms.DataGridViewBindingCompleteEventHandler(this.dataGridWorkers_DataBindingComplete);
             // 
             // dataGridSalaries
             // 
+            this.dataGridSalaries.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridSalaries.BackgroundColor = System.Drawing.SystemColors.Window;
             this.dataGridSalaries.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridSalaries.Location = new System.Drawing.Point(2, 187);
-            this.dataGridSalaries.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridSalaries.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridSalaries.Location = new System.Drawing.Point(0, 243);
+            this.dataGridSalaries.MinimumSize = new System.Drawing.Size(0, 240);
             this.dataGridSalaries.Name = "dataGridSalaries";
             this.dataGridSalaries.RowHeadersWidth = 51;
             this.dataGridSalaries.RowTemplate.Height = 29;
-            this.dataGridSalaries.Size = new System.Drawing.Size(699, 171);
+            this.dataGridSalaries.Size = new System.Drawing.Size(582, 395);
             this.dataGridSalaries.TabIndex = 2;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(704, 450);
+            this.ClientSize = new System.Drawing.Size(582, 753);
             this.Controls.Add(this.dataGridSalaries);
             this.Controls.Add(this.dataGridWorkers);
             this.Controls.Add(this.richTextBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MinimumSize = new System.Drawing.Size(400, 700);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridWorkers)).EndInit();
