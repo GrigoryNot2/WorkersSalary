a0a9ce2ae82993d6a1898b85003674dfed573483
GrigoryNot2 <65012726+GrigoryNot2@users.noreply.github.com>
2021-08-11 03:34:04 +0300

    123

--- Exchnger_netFr_sqlSrv/loginForm.Designer.cs
+++ Exchnger_netFr_sqlSrv/loginForm.Designer.cs
@@ -29,47 +29,47 @@ namespace Exchnger_netFr_sqlSrv
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox_login = new System.Windows.Forms.TextBox();
+            this.textBox_pass = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBoxLogins = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
-            // textBox1
+            // textBox_login
             // 
-            this.textBox1.Location = new System.Drawing.Point(23, 37);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(332, 20);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "User1";
+            this.textBox_login.Location = new System.Drawing.Point(23, 37);
+            this.textBox_login.Name = "textBox_login";
+            this.textBox_login.Size = new System.Drawing.Size(332, 20);
+            this.textBox_login.TabIndex = 0;
+            this.textBox_login.Text = "User1";
             // 
-            // textBox2
+            // textBox_pass
             // 
-            this.textBox2.Location = new System.Drawing.Point(23, 83);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(332, 20);
-            this.textBox2.TabIndex = 1;
-            this.textBox2.Text = "User1_pass";
+            this.textBox_pass.Location = new System.Drawing.Point(23, 83);
+            this.textBox_pass.Name = "textBox_pass";
+            this.textBox_pass.Size = new System.Drawing.Size(332, 20);
+            this.textBox_pass.TabIndex = 1;
+            this.textBox_pass.Text = "User1_pass";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(20, 21);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(38, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Text = "Логин";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(20, 67);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(45, 13);
             this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            this.label2.Text = "Пароль";
             // 
             // button1
             // 
@@ -81,27 +81,30 @@ namespace Exchnger_netFr_sqlSrv
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // comboBox1
+            // comboBoxLogins
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(248, 117);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 5;
+            this.comboBoxLogins.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxLogins.FormattingEnabled = true;
+            this.comboBoxLogins.Location = new System.Drawing.Point(129, 10);
+            this.comboBoxLogins.Name = "comboBoxLogins";
+            this.comboBoxLogins.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxLogins.TabIndex = 5;
+            this.comboBoxLogins.SelectedIndexChanged += new System.EventHandler(this.comboBoxLogins_SelectedIndexChanged);
             // 
             // loginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(381, 152);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.comboBoxLogins);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox_pass);
+            this.Controls.Add(this.textBox_login);
             this.Name = "loginForm";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.loginForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -109,12 +112,12 @@ namespace Exchnger_netFr_sqlSrv
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox_login;
+        private System.Windows.Forms.TextBox textBox_pass;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBoxLogins;
     }
 }
 
--- Exchnger_netFr_sqlSrv/loginForm.cs
+++ Exchnger_netFr_sqlSrv/loginForm.cs
@@ -13,6 +13,7 @@ namespace Exchnger_netFr_sqlSrv
 {
     public partial class loginForm : Form
     {
+        private DataTable Logins = new DataTable();
         public loginForm()
         {
             InitializeComponent();
@@ -26,82 +27,84 @@ namespace Exchnger_netFr_sqlSrv
             {
                 connection.Open();
 
-                SqlCommand command = new SqlCommand(sqlExp, connection);
+                using (SqlCommand command = new SqlCommand(sqlExp, connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
 
-                command.CommandType = CommandType.StoredProcedure;
-
-                SqlParameter userParam = new SqlParameter {
-                    ParameterName = "@s_user",
-                    Value = textBox1.Text
+                    SqlParameter userParam = new SqlParameter
+                    {
+                        ParameterName = "@s_user",
+                        Value = textBox_login.Text
                     };
-                command.Parameters.Add(userParam);
+                    command.Parameters.Add(userParam);
 
-                SqlParameter passParam = new SqlParameter
-                {
-                    ParameterName = "@s_pass",
-                    Value = textBox2.Text
-                };
-                command.Parameters.Add(passParam);
-                //SqlParameter 1 = new SqlParameter()
-                SqlParameter userdbParam = new SqlParameter
-                {
-                    ParameterName = "@s_log_db",
-                    SqlDbType = SqlDbType.NVarChar,
-                    Size = 50,
-                    Direction = ParameterDirection.Output
-                };
-                command.Parameters.Add(userdbParam);
+                    SqlParameter passParam = new SqlParameter
+                    {
+                        ParameterName = "@s_pass",
+                        Value = textBox_pass.Text
+                    };
+                    command.Parameters.Add(passParam);
+                    //SqlParameter 1 = new SqlParameter()
+                    SqlParameter userdbParam = new SqlParameter
+                    {
+                        ParameterName = "@s_log_db",
+                        SqlDbType = SqlDbType.NVarChar,
+                        Size = 50,
+                        Direction = ParameterDirection.Output
+                    };
+                    command.Parameters.Add(userdbParam);
 
-                SqlParameter passdbParam = new SqlParameter
-                {
-                    ParameterName = "@s_pass_db",
-                    SqlDbType = SqlDbType.NVarChar,
-                    Size = 50,
-                    Direction = ParameterDirection.Output
-                };
-                command.Parameters.Add(passdbParam);
+                    SqlParameter passdbParam = new SqlParameter
+                    {
+                        ParameterName = "@s_pass_db",
+                        SqlDbType = SqlDbType.NVarChar,
+                        Size = 50,
+                        Direction = ParameterDirection.Output
+                    };
+                    command.Parameters.Add(passdbParam);
 
-                SqlParameter roleParam = new SqlParameter
-                {
-                    ParameterName = "@s_role_db",
-                    SqlDbType = SqlDbType.NVarChar,
-                    Size = 50,
-                    Direction = ParameterDirection.Output
-                };
-                command.Parameters.Add(roleParam);
+                    SqlParameter roleParam = new SqlParameter
+                    {
+                        ParameterName = "@s_role_db",
+                        SqlDbType = SqlDbType.NVarChar,
+                        Size = 50,
+                        Direction = ParameterDirection.Output
+                    };
+                    command.Parameters.Add(roleParam);
 
-                SqlParameter idParam = new SqlParameter
-                {
-                    ParameterName = "@id",
-                    SqlDbType = SqlDbType.Int,
-                    Direction = ParameterDirection.Output
-                };
-                command.Parameters.Add(idParam);
+                    SqlParameter idParam = new SqlParameter
+                    {
+                        ParameterName = "@id",
+                        SqlDbType = SqlDbType.Int,
+                        Direction = ParameterDirection.Output
+                    };
+                    command.Parameters.Add(idParam);
 
-                SqlParameter nameParam = new SqlParameter
-                {
-                    ParameterName = "@name",
-                    SqlDbType = SqlDbType.NVarChar,
-                    Size = 50,
-                    Direction = ParameterDirection.Output
-                };
-                command.Parameters.Add(nameParam);
+                    SqlParameter nameParam = new SqlParameter
+                    {
+                        ParameterName = "@name",
+                        SqlDbType = SqlDbType.NVarChar,
+                        Size = 50,
+                        Direction = ParameterDirection.Output
+                    };
+                    command.Parameters.Add(nameParam);
 
-                command.ExecuteNonQuery();
+                    command.ExecuteNonQuery();
 
-                textBox1.Text = textBox2.Text = ""; //сложнее подбирать пароль вручную
+                    textBox_login.Text = textBox_pass.Text = ""; //сложнее подбирать пароль вручную
 
-                if (Convert.ToString(command.Parameters["@s_log_db"].Value) == "")
-                {
-                    MessageBox.Show("Ошибка ввода учётных данных", "Внимание");
-                    return;
+                    if (Convert.ToString(command.Parameters["@s_log_db"].Value) == "")
+                    {
+                        MessageBox.Show("Ошибка ввода учётных данных", "Внимание");
+                        return;
+                    }
+
+                    User.login = Convert.ToString(command.Parameters["@s_log_db"].Value);
+                    User.pass = Convert.ToString(command.Parameters["@s_pass_db"].Value);
+                    User.role = Convert.ToString(command.Parameters["@s_role_db"].Value);
+                    User.id = Convert.ToInt32(command.Parameters["@id"].Value);
+                    User.name = Convert.ToString(command.Parameters["@name"].Value);
                 }
-                
-                User.login = Convert.ToString(command.Parameters["@s_log_db"].Value);
-                User.pass = Convert.ToString(command.Parameters["@s_pass_db"].Value);
-                User.role = Convert.ToString(command.Parameters["@s_role_db"].Value);
-                User.id = Convert.ToInt32(command.Parameters["@id"].Value);
-                User.name = Convert.ToString(command.Parameters["@name"].Value);
                 this.Close();
 
                 //SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sql, connection);
@@ -109,6 +112,32 @@ namespace Exchnger_netFr_sqlSrv
                 //sqlDataAdapter.Fill(ds);
                 //dataGridView1.DataSource = ds.Tables[0];
             }
+        }
+
+        private void loginForm_Load(object sender, EventArgs e)
+        {
+            string connectionString = "Server = (localdb)\\MSSQLLocalDB; Database = Exchanger_contained_db; User Id = getpass; Password = getpass;";
+            string sqlExp = "pr_Logins";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlDataAdapter adapter = new SqlDataAdapter(sqlExp, connection);
+                adapter.Fill(Logins);
+
+                comboBoxLogins.DataSource = Logins;
+
+                comboBoxLogins.DisplayMember = "app_log";
+                comboBoxLogins.ValueMember = "app_log";
+               comboBoxLogins.SelectedText = "app_log";
+            }
+        }
+
+        private void comboBoxLogins_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox_login.Text = Logins.Rows[comboBoxLogins.SelectedIndex].Field<string>(0);
+            textBox_pass.Text = Logins.Rows[comboBoxLogins.SelectedIndex].Field<string>(1);
         }
     }
 }
